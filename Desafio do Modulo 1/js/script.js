7ba697dfa284a5f03d4e64a56ab35ee5b20d46a2
Raphael Pereira Valle <raphaelpereiravalle@gmail.com>
2020-05-31 22:15:43 -0300

    Atividade realizadas

---
+++ Desafio do Modulo 1/js/script.js
@@ -0,0 +1,183 @@
+'use strict';
+
+let inputName = null, btnBusca = null, statistic = null, user = null;
+let atributoArray = [];
+let filter = [];
+let numberFormat = Intl.NumberFormat("pt-BR");
+
+window.addEventListener('load', () => {
+    inputName = document.querySelector('#busca');
+    btnBusca = document.querySelector('#btnBusca');
+    statistic = document.querySelector('#statistic');
+    user = document.querySelector('#user');
+   
+    btnBusca.classList.add('disabled');
+    btnBusca.addEventListener('click', searchForName);
+    
+    fetchUrl();
+    init();
+});
+
+function preventFormSubmit() {
+    function handleFormSubmit(event) {
+      event.preventDefault();
+    }
+  
+   var form = document.querySelector('form');
+   form.addEventListener('submit', handleFormSubmit);
+ }
+
+// Funcção para carregar URL e dados para o array
+// Cabeçalho da função decorada com a palavra-chave async
+async function fetchUrl() {
+
+    const url = await fetch("https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo");
+    const json = await url.json();
+
+    atributoArray = json.results.map((data) => {
+        // Destructuring 
+        const { name, picture, dob, gender } = data;
+
+        return {
+            name: name.first + ' ' + name.last,
+            picture: picture.thumbnail,
+            age: dob.age,
+            gender: gender === 'male' ? 'M' : 'F'
+        };
+    });
+    myFunction();
+}
+
+const init = () => {
+    const onKeyUpName = (event) => {
+        let hasText = !!event.target.value && event.target.value.trim() !== '';
+
+        if(hasText){
+            btnBusca.classList.remove('disabled');            
+        }else{
+            btnBusca.classList.add('disabled');
+            loadBox();
+        }
+
+        if(hasText && event.key == 'Enter'){
+            searchForName();
+        }
+    }
+
+    inputName.addEventListener('keyup', onKeyUpName);
+    loadBox();
+}
+
+const loadBox = () =>{
+    user.innerHTML = "<h2>Nenhum usuário filtrado</h2>";
+    statistic.innerHTML = "<h2>Nada a ser exibido</h2>";
+}
+
+const searchForName = () => {
+
+    let name = inputName.value.toLowerCase().trim();
+
+    filter = atributoArray.filter(p => {
+        return p.name.toLowerCase().includes(name);
+    });
+
+    filter = sortName(filter);
+    
+    divUser(filter);
+    divStatistic(filter);
+}
+
+const sortName = () => {
+    return filter.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+    });
+}
+
+function divUser(filter) {
+
+    let bodyHTML = `<h3>${filter.length} Usúario (s) encontrado (s)</h3>`;
+     
+    filter.forEach((data, index) => {
+        
+        const { name, picture, age } = data;
+
+        const datasHtml = `<div class="card-panel grey lighten-5 z-depth-1">
+                                <div class="row valign-wrapper">
+                                    <div class="col s2">
+                                        <img class="circle responsive-img" src="${picture}"> 
+                                    </div>
+                                    <div class="col s10 font">
+                                        ${name}, ${age} anos
+                                    </div>
+                                </div>
+                            </div>`; 
+
+        bodyHTML += datasHtml;
+    });
+
+    user.innerHTML = bodyHTML;
+}
+
+function divStatistic(filter) {
+    
+    let male = countMale(filter);
+    let female = countFemale(filter);
+    let sum = sumAge(filter);
+    let average = avgAge(filter);
+
+    let bodyHTML = `<h3>Estatística</h3>`;
+
+    // Templante literals
+    const datasHtml = `<div class="card-panel lighten-5 z-depth-1">
+                            <div class="text">Sexo masculino: <strong>${male}</strong></div>
+                            <div class="text">Sexo feminino: <strong>${female}</strong></div>
+                             <div class="text">Soma das idade (s): <strong>${sum}</strong></div>
+                            <div class="text">Média das idade (s): <strong>${average}</strong></div>
+                        </div>`;
+                                                
+    statistic.innerHTML = bodyHTML += datasHtml;
+}
+
+// Cálculo o total do sexo masculino 
+function countMale(filter) {
+    const totalMale = filter.filter(person => { 
+        return person.gender === 'M'
+    }).length;
+
+    return totalMale;
+}
+
+// Cálculo o total do sexo feminino
+function countFemale(filter) {
+    const totalFemale = filter.filter(person => { 
+        return person.gender === 'F'
+    }).length;
+
+    return totalFemale;
+}
+
+// Cálculo da soma das idades
+function sumAge(filter) {
+    const totalAges = filter.reduce((accumulator, current) => {
+        return accumulator + current.age;
+    }, 0);
+
+    return totalAges;
+}
+
+// Cálculo da média das idades
+function avgAge(filter) {
+    const avgAges = filter.reduce((accumulator, current) => accumulator + current.age, 0) / filter.length;
+    return avgAges;
+    //console.log(filter.length) ;
+   // console.log(avgAges) ;
+   // console.log(avgAges / filter.length) ;
+}
+
+function myFunction() {
+    setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loading").style.display = "none";
+}
---
+++ Trabalho Prático do Módulo 1/js/script.js
@@ -0,0 +1,42 @@
+/*  function distancia(xA, xB, xC, yA, yB, yC) {
+    return Math.sqrt((xA-xB-xC)**2) + ((yA-yB-yC)**2);  
+}
+
+function quasePalindromo(palavra) {     
+    for(var i = 0; i < palavra.length / 2; i++) {
+        if (palavra[i] != palavra[palavra.length - i - 1]) 
+        return false;
+    }
+    return true;
+}
+
+function repete(vetor) {
+    var cont = 1;
+    var num = 0;
+    for(var i=1; i<vetor.length; i++) {
+        cont=0;
+        for(var j=0; j<vetor.length; j++) {
+            if (vetor[i] == vetor[j])
+            cont++;
+            num = vetor[i];
+        }
+	    return " repeticoes numero " + num + ": " + cont + " vezes";	
+	}
+}
+
+function repete(vetor) {
+    var a, i, j, c = 0;
+    console.log("%d", a);
+    var a[] = vetor;
+    for (i = 0; i < a; i++) {
+        console.log("%d", vetor[i]);
+    }
+    for (i = 0; i < a; i++) {
+         for (j = i + 1; j < a; j++) {
+             if (vetor[i] == vetor[j]) {
+                 c++;
+             }
+         }
+     }
+     return c;
+ }*/
