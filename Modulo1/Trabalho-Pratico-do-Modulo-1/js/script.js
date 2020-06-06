2e51d8f772449dc5e24210be6b9e35273b0ed912
Raphael Pereira Valle <raphaelpereiravalle@gmail.com>
2020-06-05 22:42:13 -0300

    Refatoramento dos projetos

--- Modulo1/Trabalho-Pratico-do-Modulo-1/js/script.js
+++ Modulo1/Trabalho-Pratico-do-Modulo-1/js/script.js
@@ -1,42 +1,46 @@
-/*  function distancia(xA, xB, xC, yA, yB, yC) {
-    return Math.sqrt((xA-xB-xC)**2) + ((yA-yB-yC)**2);  
+'use strict';
+
+let red = document.querySelector('#red'),
+  green = document.querySelector('#green'),
+  blue = document.querySelector('#blue'),
+  redOut = document.querySelector('#redOut'),
+  greenOut = document.querySelector('#greenOut'),
+  blueOut = document.querySelector('#blueOut');
+
+const visualizer =  document.querySelector('#visualizer');
+
+const selectColor = () => {
+  const color = `rgb(${red.value}, ${green.value}, ${blue.value})`;
+  visualizer.style.backgroundColor = color;
+  //document.querySelector('#rgbResult').innerText = newColor;         
 }
 
-function quasePalindromo(palavra) {     
-    for(var i = 0; i < palavra.length / 2; i++) {
-        if (palavra[i] != palavra[palavra.length - i - 1]) 
-        return false;
-    }
-    return true;
-}
+red.addEventListener('change', () => {
+  selectColor();
+  redOut.value = red.value;
+}, false);
 
-function repete(vetor) {
-    var cont = 1;
-    var num = 0;
-    for(var i=1; i<vetor.length; i++) {
-        cont=0;
-        for(var j=0; j<vetor.length; j++) {
-            if (vetor[i] == vetor[j])
-            cont++;
-            num = vetor[i];
-        }
-	    return " repeticoes numero " + num + ": " + cont + " vezes";	
-	}
-}
+red.addEventListener('input', () => {
+  selectColor();
+  redOut.value = red.value;
+}, false);
 
-function repete(vetor) {
-    var a, i, j, c = 0;
-    console.log("%d", a);
-    var a[] = vetor;
-    for (i = 0; i < a; i++) {
-        console.log("%d", vetor[i]);
-    }
-    for (i = 0; i < a; i++) {
-         for (j = i + 1; j < a; j++) {
-             if (vetor[i] == vetor[j]) {
-                 c++;
-             }
-         }
-     }
-     return c;
- }*/
+green.addEventListener('change', () => {
+  selectColor();
+  greenOut.value = green.value;
+}, false);
+
+green.addEventListener('input', () => {
+  selectColor();
+  greenOut.value = green.value;
+}, false);
+
+blue.addEventListener('change', () => {
+  selectColor();
+  blueOut.value = blue.value;
+}, false);
+
+blue.addEventListener('input', () => {
+  selectColor();
+  blueOut.value = blue.value;
+}, false);
